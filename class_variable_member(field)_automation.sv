8560d1b15d7cdf58db5abd2bf89186c295a2a1a7
Prabhat Chowdhary <85453914+PrabhatChowdhary@users.noreply.github.com>
2023-10-15 17:12:02 +0530

    Update class_variable_member(field)_automation.sv

--- class_variable_member(field)_automation.sv
+++ class_variable_member(field)_automation.sv
@@ -25,5 +25,7 @@ module tb;
     //`uvm_info("obj",$sformatf("value of random number is %0d",o.a),UVM_LOW); incorrect usage
     //`uvm_info("TB_TOP",$sformatf("value of random number is %0d",number.a),UVM_NONE); we want to use print macro
     number.print();
+    //number.print(uvm_default_tree_printer); to be used for printing in tree format in  place of default which is a table format
+    //number.print(uvm_default_line_printer);  to see the result in a single line
   end
 endmodule
